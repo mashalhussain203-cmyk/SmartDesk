e4dfd58194033f0486bfffb0bc1a00d886e5512e
Rimsha <ranamashalhussain@gmail.com>
2026-09-16 12:49:33 +0200

    Fix account notification email templates

---
+++ resources/views/emails/account-deleted.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Account verwijderd - SmartDesk</title>
+</head>
+<body style="font-family: Arial, sans-serif; color: #222222;">
+    <h1>Je SmartDesk-account is verwijderd</h1>
+
+    <p>Hallo {{ $name }},</p>
+
+    <p>
+        Een beheerder heeft je SmartDesk-account verwijderd.
+        Je kunt niet meer met dit account inloggen.
+    </p>
+
+    <p>
+        Had je dit niet verwacht?
+        Neem dan contact op met de beheerder van SmartDesk.
+    </p>
+
+    <p>
+        Met vriendelijke groet,<br>
+        Het SmartDesk-team
+    </p>
+</body>
+</html>
