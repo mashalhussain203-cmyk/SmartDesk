b5a9b457f62444eda0333ee015c8ce21371c91ac
mashalhussain203-cmyk <mashalhussain203@gmail.com>
2026-09-26 00:50:28 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,1 +1,17 @@
-google-site-verification=uE_1ZQTYzv2A66wPAO1UzJZCnpjh5rlAUvRzrvu2RlU
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="google-site-verification"
+          content="uE_1ZQTYzv2A66wPAO1UzJZCnpjh5rlAUvRzrvu2RlU">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ config('app.name', 'Mashal Hussain') }}</title>
+</head>
+
+<body>
+    @yield('content')
+</body>
+</html>
